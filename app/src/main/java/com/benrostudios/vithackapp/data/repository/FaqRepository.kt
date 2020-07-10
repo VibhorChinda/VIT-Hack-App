40b408ca48a9756f7f1654c066f4700e8524da9b
Hemanth Krishna <hkpdev008@gmail.com>
2020-07-10 22:19:34 +0530

    add: FAQ Repository

--- app/src/main/java/com/benrostudios/vithackapp/data/repository/FaqRepository.kt
+++ app/src/main/java/com/benrostudios/vithackapp/data/repository/FaqRepository.kt
@@ -6,4 +6,6 @@ import com.benrostudios.vithackapp.data.models.FAQ
 interface FaqRepository {
     suspend fun fetchFaqs()
     val fetchedFaqs : LiveData<List<FAQ>>
+    suspend fun postFaq(question: String)
+    val postFaqStatus: LiveData<Boolean>
 }
