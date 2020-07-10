2d88119de233953dc99a549547e46212c90d62b1
Hemanth Krishna <hkpdev008@gmail.com>
2020-07-10 21:59:17 +0530

    add: User Operation Repo

---
+++ app/src/main/java/com/benrostudios/vithackapp/data/repository/UserOperation.kt
@@ -0,0 +1,11 @@
+package com.benrostudios.vithackapp.data.repository
+
+import androidx.lifecycle.LiveData
+import com.benrostudios.vithackapp.data.models.User
+
+interface UserOperation {
+    suspend fun checkUser(uid: String)
+    suspend fun upsertUser(user: User, uid: String)
+    val userCheckStatus: LiveData<Boolean>
+    val upsertUserStatus: LiveData<Boolean>
+}
