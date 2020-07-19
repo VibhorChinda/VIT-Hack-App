3679cb4139a1d31f6420bec35cbab2198dd964cd
Vibhor Chinda <vibhorchinda@gmail.com>
2020-07-19 22:10:59 +0530

    Domain and Timeline UI done

---
+++ app/src/main/java/com/benrostudios/vithackapp/ui/home/domain/DomainViewModel.java
@@ -0,0 +1,7 @@
+package com.benrostudios.vithackapp.ui.home.domain;
+
+import androidx.lifecycle.ViewModel;
+
+public class DomainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/benrostudios/vithackapp/ui/home/problemStatement/ProblemStatementViewModel.java
@@ -0,0 +1,7 @@
+package com.benrostudios.vithackapp.ui.home.problemStatement;
+
+import androidx.lifecycle.ViewModel;
+
+public class ProblemStatementViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
