f4ff7d7afa39fe67fca5b2cbf5006c34b64d4eef
Hemanth Krishna <hkpdev008@gmail.com>
2020-07-31 11:35:58 +0530

    init: speakers fragment

---
+++ app/src/main/java/com/benrostudios/vithackapp/ui/home/speakers/Speakers.kt
@@ -0,0 +1,32 @@
+package com.benrostudios.vithackapp.ui.home.speakers
+
+import androidx.lifecycle.ViewModelProviders
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.benrostudios.vithackapp.R
+
+class Speakers : Fragment() {
+
+    companion object {
+        fun newInstance() = Speakers()
+    }
+
+    private lateinit var viewModel: SpeakersViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.speakers_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(SpeakersViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/benrostudios/vithackapp/ui/home/speakers/SpeakersViewModel.kt
@@ -0,0 +1,7 @@
+package com.benrostudios.vithackapp.ui.home.speakers
+
+import androidx.lifecycle.ViewModel
+
+class SpeakersViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
