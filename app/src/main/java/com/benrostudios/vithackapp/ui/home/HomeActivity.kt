8bdf7df8d1fd161127e2c0e27bc2c686f80637d0
Hemanth Krishna <hkpdev008@gmail.com>
2020-07-31 11:09:38 +0530

    feat: bottom nav bar

--- app/src/main/java/com/benrostudios/vithackapp/ui/home/HomeActivity.kt
+++ app/src/main/java/com/benrostudios/vithackapp/ui/home/HomeActivity.kt
@@ -2,12 +2,20 @@ package com.benrostudios.vithackapp.ui.home
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.benrostudios.vithackapp.R
+import kotlinx.android.synthetic.main.activity_home.*
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        navController = findNavController(R.id.nav_host_fragment_home_activity)
+        bottom_navigation_view_home_activity.setupWithNavController(navController)
     }
 }
